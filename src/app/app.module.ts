b1cadb7f154da92af651ef82dd44595a68c58ef0
Jeroen Heijmans <github@jeroenheijmans.nl>
2018-04-23 20:04:36 +0200

    Step 6 - the rest

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,16 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroService } from './hero.service';
+import { MessagesComponent } from './messages/messages.component';
+import { MessageService } from './message.service';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    MessagesComponent
   ],
   imports: [
     FormsModule,
@@ -21,6 +24,7 @@ import { HeroService } from './hero.service';
   ],
   providers: [
     HeroService,
+    MessageService,
   ],
   bootstrap: [AppComponent]
 })
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,13 +3,17 @@ import { HEROES } from './mock-heroes';
 import { Hero } from './hero';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
+import { MessageService } from './message.service';
 
 @Injectable()
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
+    // TODO: Do this *after* fetching
+    this.messageService.add('HeroService: fetched heroes!');
+
     return of(HEROES);
   }
 }
